03c1c6bd6f8204ab6b8ff087560f766c7d358787
Salim Bozok <salimbozok@Salim-MacBook-Pro.local>
2020-05-30 17:20:24 +0300

    static side

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,27 @@
+import { Router } from 'next/dist/client/router';
+import NProgress from 'nprogress';
+
+import 'nprogress/nprogress.css';
 import './styles.css';
+
 import { ActProvider } from '../context/ActContext';
 import { ArticleProvider } from '../context/ArticleContext';
 import { SectionProvider } from '../context/SectionContext';
 import Navbar from '../component/mevzuat/Navbar';
+
+NProgress.configure({ showSpinner: false, trickleRate: 0.1, trickleSpeed: 300 });
+
+Router.events.on('routeChangeStart', () => {
+	NProgress.start();
+})
+
+Router.events.on('routeChangeComplete', () => {
+	NProgress.done();
+})
+
+Router.events.on('routeChangeError', () => {
+	NProgress.done();
+})
 
 function MyApp({ Component, pageProps }) {
 	return (
