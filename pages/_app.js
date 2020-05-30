afcbf100e410553f522af6c74ed06fd3a1d965f6
Salim Bozok <salimbozok@Salim-MacBook-Pro.local>
2020-05-30 17:25:26 +0300

    static side

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import { Router } from 'next/dist/client/router';
 import NProgress from 'nprogress';
 
-import 'nprogress/nprogress.css';
+import './nprogress.css';
 import './styles.css';
 
 import { ActProvider } from '../context/ActContext';
