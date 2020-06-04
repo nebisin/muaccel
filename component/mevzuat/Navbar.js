823cc84bdddaaf4a0ea38a1cbaeed9e5ee57db41
Salim Bozok <salimbozok@Salim-MacBook-Pro.local>
2020-06-05 00:28:14 +0300

    logo-minor-improve

--- component/mevzuat/Navbar.js
+++ component/mevzuat/Navbar.js
@@ -1,39 +1,58 @@
 import React from 'react';
 import Link from 'next/link';
-import { useRouter } from "next/router";
+import { useRouter } from 'next/router';
 
 const Navbar = () => {
 	const router = useRouter();
 
 	const onNavbarPressed = () => {
 		let el = document.documentElement;
-		el.scrollIntoView({behavior: "smooth"});
-	}
+		el.scrollIntoView({ behavior: 'smooth' });
+	};
 
 	return (
-		<nav id="navbar" onClick={onNavbarPressed} >
+		<nav id="navbar" onClick={onNavbarPressed}>
 			<div className="container">
 				<ul className="nav-list">
-					<li>
-						<Link href="/" prefetch={false}>
+					<Link href="/" prefetch={false}>
+						<li>
 							<a>
 								<img className="logo" src="/sitelogo.png" alt="logo" />
+							</a>
+						</li>
+					</Link>
+
+					<li>
+						<Link href="/mevzuat" prefetch={false}>
+							<a
+								className={`navlink s-none ${
+									router.pathname == '/mevzuat' && 'navlink-active'
+								}`}
+							>
+								Mevzuat
 							</a>
 						</Link>
 					</li>
 					<li>
-						<Link href="/mevzuat" prefetch={false}>
-							<a className={`navlink s-none ${router.pathname == "/mevzuat" && "navlink-active"}`}>Mevzuat</a>
-						</Link>
-					</li>
-					<li>
 						<Link href="/blog" prefetch={false}>
-							<a className={`navlink s-none ${router.pathname == "/blog" && "navlink-active"}`}>Blog</a>
+							<a
+								className={`navlink s-none ${
+									router.pathname == '/blog' && 'navlink-active'
+								}`}
+							>
+								Blog
+							</a>
 						</Link>
 					</li>
 					<li>
 						<Link href="/meydan" prefetch={false}>
-							<a className={`navlink s-none ${router.pathname == "/meydan" && "navlink-active"}`}>Meydan</a>
+							<a
+								className={`navlink s-none ${
+									router.pathname == '/meydan' && 'navlink-active'
+								}`}
+							>
+								Meydan
+							</a>
 						</Link>
 					</li>
 				</ul>
