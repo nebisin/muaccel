70355fc4cd3f3e7da53ef9e7e8d0aeeb7228cf14
Salim Bozok <salimbozok@Salim-MacBook-Pro.local>
2020-06-14 01:17:50 +0300

    register

--- pages/user/register.js
+++ pages/user/register.js
@@ -1,153 +1,122 @@
-import { useFormik } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 import mevzuatApi from '../../api/mevzuat';
 
 const initialValues = {
-	username: '',
+	userName: '',
 	email: '',
 	password: '',
-	passwordconfirm: ''
+	passwordConfirmation: '',
 };
 
-const onSubmit = (values) => {
-	console.log('Form data', values);
+const onSubmit = async (values) => {
+	await mevzuatApi
+		.post('/register', {
+			userName: values.userName,
+			email: values.email,
+			password: values.password,
+		})
+		.then(function (response) {
+			localStorage.setItem('userData', JSON.stringify(response.data.token));
+		})
+		.catch(function (error) {
+			console.log(error.response.data.error);
+		})
+		.then(function () {
+			return null;
+		});
 };
 
-const validate = async (values) => {
-	let errors = {};
+const validationSchema = Yup.object({
+	userName: Yup.string()
+		.min(3, 'Kullanıcı adınız en az 3 karakterden oluşmalı!')
+		.max(36, 'Kullanıcı adınız en fazla 36 karakterden oluşabilir!')
+		.required('Bir kullanıcı adı girmelisiniz!'),
+	email: Yup.string()
+		.email('Geçerli bir e-posta adresi girmelisiniz!')
+		.required('Bir e-posta adresi girmelisiniz!'),
+	password: Yup.string()
+		.min(8, 'Şifreniz en az 8 karakterden oluşmalı!')
+		.matches(/^(?=.*\d).{1,}$/, 'Şifreniz en az bir rakam içermelidir!')
+		.matches(/^(?=.*[a-zçıöşü]).{1,}$/i, 'Şifreniz en az bir harf içermelidir!')
+		.required('Bir şifre girmelisiniz!'),
+	passwordConfirmation: Yup.string().oneOf(
+		[Yup.ref('password'), null],
+		'Girdiğiniz şifreler eşleşmiyor!'
+	),
+});
 
-	if (!values.username) {
-		errors.username = 'Bir kullanıcı adı girmelisiniz!';
-	} else if (values.username.length < 3) {
-		errors.username = 'Kullanıcı adı en az 3 karakterden oluşmalı!';
-	} else if (values.username.length > 35) {
-		errors.username = 'Kullanıcı adı en fazla 35 karakterden oluşabilir!';
-	}else {
-		const response = await mevzuatApi.post('/usercheck', { userName: values.username });
-		if(response.data.error){
-			errors.username = 'Bu kullanıcı adı kullanılıyor! Size aitse giriş yapın.'
-		}
+const validateEmail = async (value) => {
+	let error;
+
+	if (!email || !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{1,4}$/i.test(value)) {
+		return;
 	}
 
-	if (!values.email) {
-		errors.email = 'Bir e-posta adresi girmelisiniz!';
-	} else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-		errors.email = 'Geçerli bir e-posta adresi girmelisiniz!';
-	} else {
-		const response = await mevzuatApi.post('/usercheck', { email: values.email });
-		if(response.data.error){
-			errors.email = 'Bu e-posta adresi kullanılıyor! Size aitse giriş yapın.'
-		}
+	const response = await mevzuatApi.post('/usercheck', {
+		email: value,
+	});
+
+	if (response.data.error) {
+		error = 'Bu e-posta adresi kullanılıyor! Size aitse giriş yapın.';
 	}
 
-	if (!values.password) {
-		errors.password = 'Bir şifre girmelisiniz!';
-	} else if (values.password.length < 8) {
-		errors.password = 'Şifreniz en az 8 karakterden oluşmalı!';
-	} else if (!/^(?=.*\d).{1,}$/i.test(values.password)) {
-		errors.password = 'Şifreniz en az bir rakam içermelidir!';
-	} else if (!/^(?=.*[a-zçıöşü]).{1,}$/i.test(values.password)) {
-		errors.password = 'Şifreniz en az bir harf içermelidir!';
-	}
-
-	if(values.password !== values.passwordconfirm) {
-		errors.passwordconfirm = 'Girdiğiniz şifreler uyuşmuyor!'
-	}
-
-	return errors;
+	return error;
 };
 
 const RegisterPage = () => {
-	const formik = useFormik({
-		initialValues,
-		onSubmit,
-		validate,
-	});
-
 	return (
 		<div className="flex-container">
-			<form className="register-form" onSubmit={formik.handleSubmit}>
-				<div className="form-control">
-					<label htmlFor="username">Kullanıcı Adı</label>
-					<input
-						type="text"
-						id="username"
-						name="username"
-						onChange={formik.handleChange}
-						onBlur={formik.handleBlur}
-						value={formik.values.username}
-						style={
-							!formik.errors.username && formik.touched.username
-								? { borderColor: 'green' }
-								: null
-						}
-					/>
-					{formik.errors.username && formik.touched.username && (
-						<div className="error">{formik.errors.username}</div>
-					)}
-				</div>
+			<Formik
+				initialValues={initialValues}
+				validationSchema={validationSchema}
+				onSubmit={onSubmit}
+			>
+				<Form className="register-form">
+					<div className="form-control">
+						<label htmlFor="userName">Kullanıcı Adı</label>
+						<Field type="text" id="userName" name="userName" />
+						<ErrorMessage name="userName">
+							{(msg) => <div className="error">{msg}</div>}
+						</ErrorMessage>
+					</div>
 
-				<div className="form-control">
-					<label htmlFor="email">E-posta Adresi</label>
-					<input
-						type="email"
-						id="email"
-						name="email"
-						onChange={formik.handleChange}
-						onBlur={formik.handleBlur}
-						value={formik.values.email}
-						style={
-							!formik.errors.email && formik.touched.email
-								? { borderColor: 'green' }
-								: null
-						}
-					/>
-					{formik.errors.email && formik.touched.email && (
-						<div className="error">{formik.errors.email}</div>
-					)}
-				</div>
+					<div className="form-control">
+						<label htmlFor="email">E-posta Adresi</label>
+						<Field
+							type="email"
+							id="email"
+							name="email"
+							validate={validateEmail}
+						/>
+						<ErrorMessage name="email">
+							{(msg) => <div className="error">{msg}</div>}
+						</ErrorMessage>
+					</div>
 
-				<div className="form-control">
-					<label htmlFor="password">Şifre</label>
-					<input
-						type="password"
-						id="password"
-						name="password"
-						onChange={formik.handleChange}
-						onBlur={formik.handleBlur}
-						value={formik.values.password}
-						style={
-							!formik.errors.password && formik.touched.password
-								? { borderColor: 'green' }
-								: null
-						}
-					/>
-					{formik.errors.password && formik.touched.password && (
-						<div className="error">{formik.errors.password}</div>
-					)}
-				</div>
+					<div className="form-control">
+						<label htmlFor="password">Şifre</label>
+						<Field type="password" id="password" name="password" />
+						<ErrorMessage name="password">
+							{(msg) => <div className="error">{msg}</div>}
+						</ErrorMessage>
+					</div>
 
-				<div className="form-control">
-					<label htmlFor="passwordconfirm">Şifrenizi Tekrar Girin</label>
-					<input
-						type="password"
-						id="passwordconfirm"
-						name="passwordconfirm"
-						onChange={formik.handleChange}
-						onBlur={formik.handleBlur}
-						value={formik.values.passwordconfirm}
-						style={
-							!formik.errors.passwordconfirm && formik.touched.passwordconfirm
-								? { borderColor: 'green' }
-								: null
-						}
-					/>
-					{formik.errors.passwordconfirm && formik.touched.passwordconfirm && (
-						<div className="error">{formik.errors.passwordconfirm}</div>
-					)}
-				</div>
+					<div className="form-control">
+						<label htmlFor="passwordConfirmation">Şifrenizi Tekrar Girin</label>
+						<Field
+							type="password"
+							id="passwordConfirmation"
+							name="passwordConfirmation"
+						/>
+						<ErrorMessage name="passwordConfirmation">
+							{(msg) => <div className="error">{msg}</div>}
+						</ErrorMessage>
+					</div>
 
-				<button type="submit">Üye Ol</button>
-			</form>
+					<button type="submit">Üye Ol</button>
+				</Form>
+			</Formik>
 		</div>
 	);
 };
