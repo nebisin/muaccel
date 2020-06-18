2b11bd0a44f8d5b33cbb1ea37529a43e829710c4
Salim Bozok <salimbozok@Salim-MacBook-Pro.local>
2020-06-18 03:29:48 +0300

    logout

--- pages/user/logout.js
+++ pages/user/logout.js
@@ -1,17 +1,39 @@
-import BottomBar from 'component/BottomBar'
+import BottomBar from 'component/BottomBar';
 import AuthContext from 'context/AuthContext';
 import { useEffect, useContext } from 'react';
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
+import mevzuatApi from 'api/mevzuat';
 
 const LoginPage = () => {
-	const router = useRouter()
-	const {logout} = useContext(AuthContext);
+	const router = useRouter();
+	const { logout } = useContext(AuthContext);
 
 	useEffect(() => {
-		localStorage.removeItem('userData');
-		logout();
-		router.push('/');
-	}, [])
+		let storedData = JSON.parse(localStorage.getItem('userData'));
+		const apiLogout = async () => {
+			await mevzuatApi
+				.post(
+					'/logout',
+					{},
+					{
+						headers: {
+							Authorization: `Bearer ${storedData}`,
+						},
+					}
+				)
+				.then(function (response) {
+					localStorage.removeItem('userData');
+					logout();
+					router.push('/');
+				})
+				.catch(function (error) {
+					localStorage.removeItem('userData');
+					logout();
+					router.push('/');
+				});
+		};
+		apiLogout();
+	}, []);
 	return (
 		<div>
 			<div>Çıkış yapılıyor...</div>
