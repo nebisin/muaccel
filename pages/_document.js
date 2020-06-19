2fbc36890700af66a59298fb412d5341c33335c7
Salim Bozok <salimbozok@Salim-MacBook-Pro.local>
2020-06-19 19:35:11 +0300

    googleAnalize

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,21 @@ class MyDocument extends Document {
 	render() {
 		return (
 			<Html lang="tr">
-				<Head />
+				<Head>
+					<script
+						async
+						src="https://www.googletagmanager.com/gtag/js?id=UA-148977213-1"
+					/>
+					<script
+						dangerouslySetInnerHTML={{
+							__html: `window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-148977213-1');`,
+						}}
+					/>
+				</Head>
 				<body>
 					<Main />
 					<NextScript />
