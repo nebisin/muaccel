ee9968105d54bd9580f3737a1f72b2a52d28756d
Salim Bozok <salimbozok@Salim-MacBook-Pro.local>
2020-06-21 20:36:43 +0300

    user-side-bar

--- component/user/UserSidebar.js
+++ component/user/UserSidebar.js
@@ -1,19 +1,34 @@
 import React from 'react';
+import Link from 'next/link';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-	faUserCircle,
-} from '@fortawesome/free-regular-svg-icons';
+import { faUserCircle } from '@fortawesome/free-regular-svg-icons';
 
 const UserSidebar = () => {
 	return (
 		<aside id="sidebar" className="h-none">
-			<h2 className="title">
-				<FontAwesomeIcon
-					icon={faUserCircle}
-					className='sidebar-icon'
-				/>
-				Hesabım
-			</h2>
+			<div className="user-sidebar-content">
+				<div className="user-side-link user-side-link-active">
+					<Link href="">
+						<a>Notlarım</a>
+					</Link>
+				</div>
+				<div className="user-side-link">
+					<Link href="">
+						<a>Favorilerim</a>
+					</Link>
+				</div>
+				<div className="user-side-link">
+					<Link href="">
+						<a>Tartışmalarım</a>
+					</Link>
+				</div>
+				<div className="user-side-link">
+					<Link href="">
+						<a>Bloglarım</a>
+					</Link>
+				</div>
+			</div>
 		</aside>
 	);
 };
