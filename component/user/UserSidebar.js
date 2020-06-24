ee804f1c8d82cadfccfb4dfa6ce3f156891e1117
Salim Bozok <salimbozok@Salim-MacBook-Pro.local>
2020-06-25 02:34:40 +0300

    onclick

--- component/user/UserSidebar.js
+++ component/user/UserSidebar.js
@@ -9,28 +9,28 @@ const UserSidebar = ({ current, setCurrent }) => {
 						current === 0 && 'user-side-link-active'
 					}`}
 				>
-					<span onMouseDown={() => setCurrent(0)}>Notlarım</span>
+					<span onClick={() => setCurrent(0)}>Notlarım</span>
 				</div>
 				<div
 					className={`user-side-link ${
 						current === 1 && 'user-side-link-active'
 					}`}
 				>
-					<span onMouseDown={() => setCurrent(1)}>Favorilerim</span>
+					<span onClick={() => setCurrent(1)}>Favorilerim</span>
 				</div>
 				<div
 					className={`user-side-link ${
 						current === 2 && 'user-side-link-active'
 					}`}
 				>
-					<span onMouseDown={() => setCurrent(2)}>Tartışmalarım</span>
+					<span onClick={() => setCurrent(2)}>Tartışmalarım</span>
 				</div>
 				<div
 					className={`user-side-link ${
 						current === 3 && 'user-side-link-active'
 					}`}
 				>
-					<span onMouseDown={() => setCurrent(3)}>Bloglarım</span>
+					<span onClick={() => setCurrent(3)}>Bloglarım</span>
 				</div>
 			</div>
 		</aside>
