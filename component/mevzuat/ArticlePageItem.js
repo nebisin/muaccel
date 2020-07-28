153b4fd73f83e11de5c34e6276a975526b2857a5
Salim Bozok <salimbozok@Salim-MacBook-Pro.local>
2020-07-28 15:25:31 +0300

    article-csr

--- component/mevzuat/ArticlePageItem.js
+++ component/mevzuat/ArticlePageItem.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import mevzuatApi from 'api/mevzuat';
+
 import Favorite from './Favorite';
 
 const ArticleItem = ({ item }) => {
-	if (item.content !== undefined) {
+	const [article, setArticle] = useState(item);
+
+	useEffect(() => {
+		const getArticle = async (item) => {
+			if(item){
+				const response = await mevzuatApi.get('/article', { params: { id: item._id } });
+				setArticle(response.data.article);
+			}
+		}
+		getArticle(item);
+	}, [item])
+
+	if (article.content !== undefined) {
 		return (
 			<article className="article-page-card">
 				<div className="article-page-card-header">
-					<h2>Madde {item.title}</h2>
-					<h1>{item.name}</h1>
+					<h2>Madde {article.title}</h2>
+					<h1>{article.name}</h1>
 				</div>
-				<div className="article-page-card-content">{item.content}</div>
-				<Favorite position="top" articleId={item._id} />
+				<div className="article-page-card-content">{article.content}</div>
+				<Favorite position="top" articleId={article._id} />
 			</article>
 		);
 	} else {
