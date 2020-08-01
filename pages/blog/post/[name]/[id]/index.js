c96c24f3740e09c8c46264c534c4569f5ee509f5
Salim Bozok <salimbozok@Salim-MacBook-Pro.local>
2020-08-01 22:32:44 +0300

    showpost

--- pages/blog/post/[name]/[id]/index.js
+++ pages/blog/post/[name]/[id]/index.js
@@ -6,65 +6,57 @@ import ReadOnly from 'component/draft/ReadOnly';
 import UserWidget from 'component/user/UserWidget';
 
 const ShowPost = ({ data }) => {
-	const [editorState, setEditorState] = useState();
-
-	useEffect(() => {
-		if (data?.content) {
-			const rawContentFromStore = convertFromRaw(JSON.parse(data.content));
-			setEditorState(EditorState.createWithContent(rawContentFromStore));
-		}
-	}, [data]);
+	const rawContentFromStore = convertFromRaw(JSON.parse(data.content));
+	const [editorState, setEditorState] = useState(
+		EditorState.createWithContent(rawContentFromStore)
+	);
 
 	return (
 		<React.Fragment>
-			{data?.content && (
-				<React.Fragment>
-					<Head>
-						<title>{data.title} | Muaccel Blog</title>
-						<meta name="description" content={data.abstract} />
-						<meta property="og:title" content={data.title} />
-						<meta property="og:description" content={data.abstract} />
-						<meta
-							property="og:image"
-							content="https://www.muaccel.com/blog-hero-photo.jpg"
-						/>
-					</Head>
-					<div className="blog-container">
-						<div className="create-blog-container">
-							<div className="create-blog-section">
-								<div className="post-preview">
-									<h1 className="blog-post-title">{data.title}</h1>
+			<Head>
+				<title>{data.title} | Muaccel Blog</title>
+				<meta name="description" content={data.abstract} />
+				<meta property="og:title" content={data.title} />
+				<meta property="og:description" content={data.abstract} />
+				<meta
+					property="og:image"
+					content="https://www.muaccel.com/blog-hero-photo.jpg"
+				/>
+			</Head>
+			<div className="blog-container">
+				<div className="create-blog-container">
+					<div className="create-blog-section">
+						<div className="post-preview">
+							<h1 className="blog-post-title">{data.title}</h1>
 
-									<div className="author-preview">
-										<a className="author-preview-logo"></a>
-										<a className="author-preview-name">
-											<div className="author-preview-displayname">
-												{data.author.name}
-											</div>
-											<div className="author-preview-username">
-												@{data.author.userName}
-											</div>
-										</a>
+							<div className="author-preview">
+								<a className="author-preview-logo"></a>
+								<a className="author-preview-name">
+									<div className="author-preview-displayname">
+										{data.author.name}
 									</div>
-
-									<div className="blog-post-abstract">{data.abstract}</div>
-									<div className="blog-post-content">
-										{editorState && (
-											<ReadOnly
-												editorState={editorState}
-												setEditorState={setEditorState}
-											/>
-										)}
+									<div className="author-preview-username">
+										@{data.author.userName}
 									</div>
-								</div>
+								</a>
 							</div>
-							<div className="create-blog-sidebar">
-								<UserWidget user={data.author} />
+
+							<div className="blog-post-abstract">{data.abstract}</div>
+							<div className="blog-post-content">
+								{editorState && (
+									<ReadOnly
+										editorState={editorState}
+										setEditorState={setEditorState}
+									/>
+								)}
 							</div>
 						</div>
 					</div>
-				</React.Fragment>
-			)}
+					<div className="create-blog-sidebar">
+						<UserWidget user={data.author} />
+					</div>
+				</div>
+			</div>
 		</React.Fragment>
 	);
 };
@@ -89,7 +81,7 @@ export async function getStaticProps({ params }) {
 	const response = await mevzuatApi.get(`/blog/${id}`);
 	const data = response.data;
 
-	return { props: { data }, unstable_revalidate: 10, };
+	return { props: { data }, unstable_revalidate: 10 };
 }
 
 export default ShowPost;
