cf8c6b22d9d0a43897d3092e8f66185ace9a3fd7
Salim Bozok <salimbozok@Salim-MacBook-Pro.local>
2020-08-27 12:43:28 +0300

    go article

--- component/mevzuat/GoArticle.js
+++ component/mevzuat/GoArticle.js
@@ -1,15 +1,43 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
+
 import ArticleContext from 'context/ArticleContext';
+import mevzuatApi from 'api/mevzuat';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faTimes, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 const GoArticle = ({ id }) => {
 	const [term, setTerm] = useState('');
 	const [isLoading, setIsLoading] = useState(false);
+	const [listLoading, setListLoading] = useState(false);
+	const [articleList, setArticleList] = useState();
 	const [focused, setFocused] = useState(false);
 	const { getArticleByTitle } = useContext(ArticleContext);
 	let router = useRouter();
+
+	const getList = async (search, searchId) => {
+		if (search != null && searchId != null) {
+			try {
+				setListLoading(true);
+				const response = await mevzuatApi.post('/articles', {
+					search: search,
+					searchId: searchId,
+				});
+				setArticleList(response.data);
+				setListLoading(false);
+			} catch (error) {
+				setListLoading(false);
+				console.log(
+					'Bir şeyler ters gitti. Lütfen daha sonra tekrar deneyiniz.'
+				);
+			}
+		}
+	};
+
+	useEffect(() => {
+		getList(term, id);
+	}, [id]);
 
 	const handleChange = (event) => {
 		setTerm(event.target.value);
@@ -17,7 +45,11 @@ const GoArticle = ({ id }) => {
 
 	const handleSubmit = (event) => {
 		event.preventDefault();
+		getList(term, id);
+	};
 
+	const handleSubmitAlternative = (event) => {
+		event.preventDefault();
 		if (!term) {
 			alert('Bir madde numarası girmelisiniz!');
 			return;
@@ -40,6 +72,7 @@ const GoArticle = ({ id }) => {
 						article.actId._id
 					}/madde/${article.title.replace(/\//g, '-')}`
 				);
+				setFocused(false);
 				setIsLoading(false);
 			} catch (e) {
 				alert('Aradığınız madde bulunamadı!');
@@ -62,32 +95,86 @@ const GoArticle = ({ id }) => {
 					<div className="others-full">
 						<div className="others-full-inside">
 							<div className="others-full-insider">
-								<div>
-									<h2>Maddeler</h2>
-									<FontAwesomeIcon
-										icon={faTimes}
-										className="close-others"
-										onClick={() => setFocused(false)}
-									/>
+								<div className="others-insider-top">
+									<div className="others-insider-title">
+										<h2>
+											Maddeler{' '}
+											{listLoading ? (
+												<FontAwesomeIcon
+													icon={faSpinner}
+													className="login-spinner"
+												/>
+											) : (
+												''
+											)}
+										</h2>
+									</div>
+									<div className="close-others-div">
+										<FontAwesomeIcon
+											icon={faTimes}
+											className="close-others"
+											onClick={() => setFocused(false)}
+										/>
+									</div>
 								</div>
 								<div className="others-go-form">
-									<form onSubmit={handleSubmit} className="others-form">
-										<input
-											className="others-input"
-											type="search"
-											placeholder="Madde Ara"
-											value={term || ''}
-											onChange={handleChange}
-											disabled={isLoading}
-										/>
-										<input
+									<div className="others-form">
+										<form onSubmit={handleSubmit} className="others-input">
+											<input
+												className="others-input-inside"
+												type="search"
+												placeholder="Madde Ara"
+												value={term || ''}
+												onChange={handleChange}
+												disabled={isLoading}
+											/>
+										</form>
+										<button
 											className="others-button"
-											type="submit"
-											value="Git"
 											disabled={isLoading}
-										/>
-									</form>
+											onClick={handleSubmitAlternative}
+										>
+											Git
+										</button>
+									</div>
 								</div>
+								{articleList && (
+									<div className="article-text-list-container">
+										<div className="article-text-list">
+											<div className="article-text-list-left">
+												<b>Numarası</b>
+											</div>
+											<div className="article-text-list-right">
+												<b>Başlığı</b>
+											</div>
+										</div>
+										{articleList.map((article) => (
+											<Link
+												href="/mevzuat/[actName]/[id]/madde/[title]"
+												as={`/mevzuat/${article.actId.name
+													.replace(/\s/g, '-')
+													.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, '')}/${
+													article.actId._id
+												}/madde/${article.title.replace(/\//g, '-')}`}
+												key={article._id}
+											>
+												<a>
+													<div
+														className="article-text-list"
+														onClick={() => setFocused(false)}
+													>
+														<div className="article-text-list-left">
+															Madde {article.title}
+														</div>
+														<div className="article-text-list-right">
+															{article.name}
+														</div>
+													</div>
+												</a>
+											</Link>
+										))}
+									</div>
+								)}
 							</div>
 						</div>
 					</div>
@@ -99,30 +186,6 @@ const GoArticle = ({ id }) => {
 			)}
 		</React.Fragment>
 	);
-	/*
-	return (
-		<div className={`others-go ${focused && 'others-go-focused'}`}>
-			<form onSubmit={handleSubmit} className="others-form">
-				<input
-					className="others-input"
-					type="search"
-					placeholder="Madde"
-					value={term || ''}
-					onChange={handleChange}
-					disabled={isLoading}
-					onFocus={() => setFocused(true)}
-					onBlur={() => setFocused(false)}
-				/>
-				<input
-					className="others-button"
-					type="submit"
-					value="Git"
-					disabled={isLoading}
-				/>
-			</form>
-		</div>
-	);
-*/
 };
 
 export default GoArticle;
