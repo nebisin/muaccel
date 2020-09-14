403b378d0e8bdfb3a68ca8c0e8b9b0f6dbd8ac94
Salim Bozok <salimbozok@Salim-MacBook-Pro.local>
2020-09-14 21:36:13 +0300

    allblogs

--- component/user/AllBlogs.js
+++ component/user/AllBlogs.js
@@ -1,59 +1,66 @@
 import { useEffect, useState } from 'react';
 import mevzuatApi from 'api/mevzuat';
 import BlogPosts from 'component/blog/BlogPosts';
+import InfiniteScroll from 'react-infinite-scroller';
+import ArticleHolder from 'component/mevzuat/ArticleHolder';
 
 const AllBlogs = ({ user, token }) => {
 	const [blogs, setBlogs] = useState([]);
-	const [isLoading, setIsLoading] = useState(true);
+	const [hasMore, setHasMore] = useState(true);
 
-	useEffect(() => {
-		const getAll = async (token) => {
-			const response = await mevzuatApi.post(
-				'/user/blogs',
-				{
-					sort: { createdAt: -1 },
+	const getBlogs = async (token, page) => {
+		const response = await mevzuatApi.post(
+			'/user/blogs',
+			{
+				limit: 3,
+				skip: (page - 1) * 3,
+				sort: { createdAt: -1 },
+			},
+			{
+				headers: {
+					Authorization: `Bearer ${token}`,
 				},
-				{
-					headers: {
-						Authorization: `Bearer ${token}`,
-					},
-				}
-			);
-			setIsLoading(false);
-			if (response.data) {
-				setBlogs(response.data);
 			}
-		};
+		);
 
-		if (user && token) {
-			getAll(token);
+		if (response.data) {
+			if (response.data.length < 3) {
+				setHasMore(false);
+			}
+			setBlogs((blogs) => blogs.concat(response.data));
 		}
-	}, [user, token]);
+	};
 
 	return (
 		<div className="user-favorite-container create-blog-section">
-			{!isLoading ? (
-				blogs.length ? (
+			<InfiniteScroll
+				pageStart={0}
+				loadMore={(page) => getBlogs(token, page)}
+				hasMore={hasMore}
+				loader={<ArticleHolder key={0} />}
+				threshold={800}
+			>
+				{blogs.length ? (
 					<BlogPosts data={blogs} />
 				) : (
-					<div className="user-no-favorite">
-						<div className="user-no-favorite-description">
-							Henüz hiç blog oluşturmamışsınız.
-						</div>
-						<div className="user-no-favorite-image-container fade-in">
-							<img
-								className="user-no-favorite-image"
-								src="/noblog.png"
-								alt="blog"
-							/>
-						</div>
-					</div>
-				)
-			) : (
-				<div style={{ width: 'auto', display: 'flex', marginBottom: '20px', minHeight: '700px' }}>
-					<div className="loader">Loading...</div>
-				</div>
-			)}
+					<React.Fragment>
+						{!hasMore && (
+							<div className="user-no-favorite">
+								<div className="user-no-favorite-description">
+									Henüz hiç blog oluşturmamışsınız.
+								</div>
+								<div className="user-no-favorite-image-container fade-in">
+									<img
+										className="user-no-favorite-image"
+										src="/noblog.png"
+										alt="blog"
+									/>
+								</div>
+							</div>
+						)}
+					</React.Fragment>
+				)}
+			</InfiniteScroll>
 		</div>
 	);
 };
