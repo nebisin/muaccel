945ec5af7947b4b40e93d55e2c0150f2fef70a2a
Salim Bozok <salimbozok@Salim-MacBook-Pro.local>
2020-10-05 15:18:07 +0300

    ads test

--- component/user/Dashboard.js
+++ component/user/Dashboard.js
@@ -17,9 +17,9 @@ const Dashboard = () => {
 			</aside>
 			<section className="dashboard-main">
 				<ActiveAlert />
-				<CreateStatus />
+				{/*<CreateStatus />*/}
 				<DashboardActs />
-				<StatusItem />
+				{/*<StatusItem />*/}
 				<DashboardArticles />
 				<DashboardBlogs />
 				<Footer />
