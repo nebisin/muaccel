2f56f7c70d7690de5ec8bc5ed8e7c7dcf9abc3f6
Salim Bozok <salimbozok@Salim-MacBook-Pro.local>
2020-10-05 16:34:00 +0300

    ads

--- component/ads/SidebarAds.js
+++ component/ads/SidebarAds.js
@@ -1,20 +1,25 @@
+import { useEffect } from 'react';
+
 const SidebarAds = () => {
+	useEffect(() => {
+		try {
+			(window.adsbygoogle = window.adsbygoogle || []).push({});
+		} catch (err) {
+			console.log(err);
+		}
+	}, []);
+
 	return (
 		<div
 			dangerouslySetInnerHTML={{
-				__html: `<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <!-- sidebar-kare -->
-    <ins class="adsbygoogle"
+				__html: `<ins class="adsbygoogle"
          style="display:block"
          data-ad-client="ca-pub-1639338975133942"
          data-ad-slot="3073578295"
          data-ad-format="auto"
-         data-full-width-responsive="true"></ins>
-    <script>
-         (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>`,
-            }}
-            style={{maxWidth: '340px'}}
+         data-full-width-responsive="true"></ins>`,
+			}}
+			style={{ maxWidth: '340px', padding: '10px', display: 'block' }}
 		/>
 	);
 };
